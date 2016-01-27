705534e8c2866236603da635984a6abd20616410
Peter Schulam <pschulam@gmail.com>
2016-01-27 14:56:36 -0500

    Add helper functions to sclerodb.

--- python/sclerodb.py
+++ python/sclerodb.py
@@ -19,10 +19,29 @@ def connection(user, pw):
     return pymssql.connect(_SERVER, _DOMAIN + user, pw, _DATABASE)
 
 
-def get_table(conn, table, as_dict=True):
-    """Yield all rows of a table in the sclerodata database."""
-    query = 'select * from {}'.format(table)
-    with conn.cursor(as_dict=as_dict) as cursor:
+def run_query(conn, query):
+    """Yield all rows of a query run against the database."""
+    with conn.cursor(as_dict=True) as cursor:
         cursor.execute(query)
         for row in cursor:
             yield row
+
+
+def tables(conn):
+    """Get the names of all tables."""
+    table_info = run_query(conn, 'select * from information_schema.tables;')
+    return [row['TABLE_NAME'] for row in table_info]
+
+
+def columns(conn, table):
+    """Get the information on columns of a table."""
+    query = 'select * from information_schema.columns'
+    for row in run_query(conn, query):
+        if row['TABLE_NAME'] == table:
+            yield row
+
+
+def get_table(conn, table):
+    """Fetch an entire table."""
+    query = 'select * from {}'.format(table)
+    return run_query(conn, query)
